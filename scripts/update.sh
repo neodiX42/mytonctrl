554d035f0e7bdb0bddca1172b4b6aa260e610c58
neodiX <neodix@ton.org>
2023-09-04 10:50:52 +0200

    migrate /usr/local/bin to /root/.local/share

--- scripts/update.sh
+++ scripts/update.sh
@@ -45,12 +45,35 @@ echo "https://github.com/${author}/${repo}.git -> ${branch}"
 git clone --recursive https://github.com/${author}/${repo}.git
 cd ${repo} && git checkout ${branch} && git submodule update --init --recursive
 
+# migrate old locations for root user
 if [ -f "/usr/local/bin/mytoncore/mytoncore.db" ]; then
-  # migrate old locations
-  # todo
-  echo Migrating old...
+  echo "Migrating /usr/local/bin/ to $(echo ~root/.local/share/)"
+
+  #wallets
+  if [ ! -d "~root/.local/share/mytoncore/wallets/" ]; then
+    mkdir -p ~root/.local/share/mytoncore/wallets
+  fi
+  cd ~root/.local/share/mytoncore/wallets/
+  for fn in *; do mv $fn $fn.backup; done
+  for fn in /usr/local/bin/mytoncore/wallets/*; do cp $fn $(basename $fn); done
+
+  #pools
+  if [ ! -d "~root/.local/share/mytoncore/pools/" ]; then
+    mkdir -p ~root/.local/share/mytoncore/pools
+  fi
+  cd ~root/.local/share/mytoncore/pool/
+  for fn in *; do mv $fn $fn.backup; done
+  for fn in /usr/local/bin/mytoncore/pool/*; do cp $fn $(basename $fn); done
+
+  #contracts
+  if [ ! -d "~root/.local/share/mytoncore/contracts/" ]; then
+    mkdir -p ~root/.local/share/mytoncore/contracts
+  fi
+  cd ~root/.local/share/mytoncore/contracts/
+  for fn in *; do mv $fn $fn.backup; done
+  for fn in /usr/local/bin/mytoncore/contracts/*; do cp -R $fn $(basename $fn); done
 fi
-#migrate to ~/.local/share
+
 if [[ "$OSTYPE" =~ darwin.* ]]; then
   kickstart -k system/mytoncore
 else
